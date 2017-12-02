2d6dd99b049a3ccfc75ab7c525bf16c54cf8657c
sanjadjak <sanya.dinic@gmail.com>
2017-12-02 07:43:07 -0800

    pause instead of stop

--- js/pomodorski.js
+++ js/pomodorski.js
@@ -1,1 +1,55 @@
+var startButton = document.getElementById("start");
+var pauseButton = document.getElementById("pause");
+var resetButton = document.getElementById("reset");
+var minsLeft = document.getElementById("mins-left");
+var secsLeft = document.getElementById("secs-left");
 
+var start = function() {
+    console.log("start");
+    begin = setInterval(countdown, 1000); // Call countdown function every 1000 milliseconds
+    startButton.disabled = true;
+    stopButton.disabled = false;
+}
+
+var pause = function() {
+    console.log("pause");
+    window.clearInterval(begin) // clear the timer and so stop the clock
+    stopButton.disabled = true;
+    startButton.disabled = false;
+}
+
+var reset = function() {
+    console.log("reset");
+    window.clearInterval(begin) // clear the timer and so stop the clock
+    secsLeft.innerText = 00;
+    minsLeft.innerText = document.getElementById("mins").innerText;
+    stopButton.disabled = true;
+    startButton.disabled = false;
+}
+
+// This is what makes the clock tick downwards
+var countdown = function() {
+    console.log("countdown");
+    if (secsLeft.innerText <= 0) {
+        secsLeft.innerText = 60;
+        minsLeft.innerText--;
+    };
+    secsLeft.innerText--;
+
+    if (secsLeft.innerText <= 0 && minsLeft.innerText <= 0) {
+        window.clearInterval(begin);
+        var audio = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/11525/alarm.wav');
+        audio.play();
+    };
+}
+
+var more = function() {
+    document.getElementById("mins").innerText++;
+    minsLeft.innerText = document.getElementById("mins").innerText;
+}
+
+var less = function() {
+    document.getElementById("mins").innerText--;
+    if (document.getElementById("mins").innerText <= 1) { document.getElementById("mins").innerText = 1 };
+    minsLeft.innerText = document.getElementById("mins").innerText;
+}
